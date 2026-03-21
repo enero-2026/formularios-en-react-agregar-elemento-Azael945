5601b0169fe80bffea68f508580ffe23a7023d13
Azael945 <azael.rodriguezr@uanl.edu.mx>
2026-03-21 11:22:26 -0600

    Delete app directory

--- app/(tabs)/perfil.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-import { useLocalSearchParams } from 'expo-router';
-
-export default function Perfil() {
-    return(
-        <Text>Perfil</Text>
-    )
-}
